1c6d95acb0da384fe727615ec9346b61128ad3ef
ümit çelebi <53602829+umitcelebi@users.noreply.github.com>
2021-06-06 11:34:24 +0300

    Add project files.

---
+++ UserDirectory.WebUI/Models/Model.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserDirectory.WebUI.Models
+{
+    public class Model
+    {
+    }
+}
